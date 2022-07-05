db69d9c8e538460ae95091a43fb31f383486ac14
venkat <srivenkatsairaj13@gmail.com>
2022-07-05 22:36:58 +0530

    dom ex2 start

---
+++ begginer-dom-exercies/dom2/app.js
@@ -0,0 +1,43 @@
+/* JavaScript DOM Exercises 02 Tutorial: https://youtu.be/qQy5K-pE8Fo */
+/*
+  Exercises 01
+  ------------
+  Add a label to each of the input fields: username, password, confirm password
+*/
+
+
+/*
+  Exercise 02
+  -----------
+  Add a required validation to each input that shows an error message next to the entry if it does not have any text entered.
+*/
+
+
+/*
+  Exercise 03
+  -----------
+  Add a further validation to check if the user input in the password and confirm password inputs match.  Show an error message if they do not.
+*/
+
+/*
+  Exercise 04
+  -----------
+Ensure the ‘Register’ button is disabled until the user has entered valid data into all the input fields.  Once they have, the registration button should then be enabled.
+*/
+
+
+/*
+  Exercise 05
+  -----------
+When the user clicks the ‘Register’ button, a message should be displayed informing them of a successful user registration.
+*/
+
+
+
+/*
+  Exercise 02
+  -----------
+  Add a required validation to each input that shows an error message next to the entry if it does not have any text entered.
+*/
+
+
