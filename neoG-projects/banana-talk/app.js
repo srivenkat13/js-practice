0c28c34315272511150d59ee4af503b13b82f021
venkat <srivenkatsairaj13@gmail.com>
2022-07-15 00:17:16 +0530

    add js

---
+++ neoG-projects/banana-talk/app.js
@@ -0,0 +1,28 @@
+const inputText = document.getElementById("translate-input");
+const btn = document.getElementById("translate-button");
+btn.addEventListener("click", getTranslatedText);
+const translatedText = document.getElementById("translate-output");
+
+const url = "https://api.funtranslations.com/translate/minion.json";
+
+function getTranslatedText() {
+  let input = inputText.value;
+  let completeUrl = constructURL(input);
+
+  fetch(completeUrl)
+    .then((Response) => Response.json())
+    .then((data) => (translatedText.innerText = data.contents.translatedText))
+    .catch((error) => {
+      alert("some error occured with API", error);
+      //   console.error("API ratelimit is reached");
+    });
+}
+
+function constructURL(inp) {
+  if (inp) {
+    let encodedURI = encodeURI(inp);
+    return `${url}?text=${encodedURI}`;
+  } else {
+    alert("Oye, enter some text");
+  }
+}
